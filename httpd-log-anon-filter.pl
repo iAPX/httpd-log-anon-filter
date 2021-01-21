fb997f05ea0b3b2903512f33f2332819e41c5f9e
Philippe Vigier <870564+iAPX@users.noreply.github.com>
2021-01-21 15:35:12 -0500

    Use sha256 instead md5, and increase random to 256 bits
    Ensure we are on the safe-side against high-level actors

--- httpd-log-anon-filter.pl
+++ httpd-log-anon-filter.pl
@@ -20,43 +20,44 @@
 use strict;
 use warnings;
 
-use Digest::MD5 qw(md5);
+use String::Random qw(random_regex random_string);
+use Digest::SHA qw(sha256);
+
 
 my $logfile = shift @ARGV || die  'no output file given';
 
 open my $log_fh, '>>', $logfile or die "can't open `$logfile': $!\n";
 $log_fh->autoflush();
 
-# @TODO sha2 might replace md5
-# get random MD5 64 bits salt
+# sha256 version with 256 bits random
 # this will give a new salt on every invocation, meaning that the
 # hashes are 'new' after logrotate's daily 'apache reload'
-my $salt = chr(rand(256)) . chr(rand(256)) . chr(rand(256)) . chr(rand(256)) . chr(rand(256)) . chr(rand(256)) . chr(rand(256)) . chr(rand(256));
+my $salt = random_string( "b" x 32 );
 
 while (my $line = <STDIN>) {
     my ($ip, $tail) = split /\s+/, $line, 2;
 
-    # convert salt plus hostname field contents to md5 hash
-    my $md5 = md5( $salt . $ip );
+    # convert salt plus hostname field contents to sha256 hash
+    my $hash = sha256( $salt . $ip );
     
     if ($ip =~ /:/) {
 	# host field looks like IPv6:
-	# convert complete md5 hash to an IPv6 address
-	$ip = join( ':', unpack( '(H4)8', $md5));
+	# convert complete sha256 hash to an IPv6 address
+	$ip = join( ':', unpack( '(H4)8', $hash));
 
 	# generate "documentation" addresses: 2001:db8::/32
-	# $ip = '2001:db8:' . join( ':', unpack( '(H4)6', $md5));
+	# $ip = '2001:db8:' . join( ':', unpack( '(H4)6', $hash));
 
 	# generate discard addresses? 0100::/64
-	# $ip = '0100::' . join( ':', unpack( '(H4)4', $md5));
+	# $ip = '0100::' . join( ':', unpack( '(H4)4', $hash));
     }
     else {
 	# host field contains IPv4, resolved hostname or any other junk:
-	# convert first 4 bytes of md5 hash to an IPv4 address
-	$ip = join( '.', unpack( 'C4', $md5));
+	# convert first 4 bytes of hash to an IPv4 address
+	$ip = join( '.', unpack( 'C4', $hash));
 
 	# generate IPs in local pool (use 10.0.0.0/8 because it's the biggest local range)
-	# $ip = '10.' . join( '.', unpack( 'C3', $md5));
+	# $ip = '10.' . join( '.', unpack( 'C3', $hash));
     }
     print $log_fh "$ip $tail";
 }
